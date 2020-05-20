acfc6dc32d5bf8ca820ec44719f867745b7cdcae
privera <privera@celerity.com>
2020-05-19 21:16:03 -0400

    fix models syntax error

--- github_actions/website/models.py
+++ github_actions/website/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Page(models.Model):
     name = models.CharField(unique=True)
-    slug - models.SlugField(unique=True)
+    slug = models.SlugField(unique=True)
 
     def __str__(self):
         return self.name
