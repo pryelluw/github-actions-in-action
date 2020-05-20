3b126a587815cf4b46a17ad87186b6a7154f1125
privera <privera@celerity.com>
2020-05-19 21:20:32 -0400

    fix models syntax error

--- github_actions/website/tests.py
+++ github_actions/website/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from website.models import Page
 
 
-class WebsiteTests(Testcase):
+class WebsiteTests(TestCase):
     def test_page_is_created(self):
         page = Page.objects.create(name="Test Page", slug="test-page")
         self.assertTrue(page)
